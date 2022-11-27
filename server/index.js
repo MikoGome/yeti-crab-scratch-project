4b668d45fc08e8cdcd695e7fa311ad86002a0216
Miko <89363492+MikoGome@users.noreply.github.com>
2022-11-27 02:04:42 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,14 +16,18 @@ const cookieParser = require('cookie-parser');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '..', 'build')));
 
 //when the path is /api, go into the apiRouter.js in routes folder
 app.use('/api', apiRouter);
 
 //When the client makes a GET request to homepage, send back the index.html
 app.get('/', (req, res) => {
-  return res.sendFile(path.join(__dirname, '..', 'build', 'index.html');
+  return res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+//When the client makes a GET request to bundle.js, send back the bundle.js
+app.get('/bundle.js', (req, res) => {
+  return res.sendFile(path.join(__dirname, '..', 'build', 'bundle.js'));
 });
 
 //404 NOT FOUND, unknown path handler
